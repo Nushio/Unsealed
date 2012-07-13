ade08d159b8f91b06a91f286a63ad4577c280878
unknown <drawig@gmail.com>
2012-07-13 21:32:13 +0200

    Added a base class for all map objects.

---
+++ Unsealed/src/net/k3rnel/unsealed/objects/MapObject.java
@@ -0,0 +1,58 @@
+package net.k3rnel.unsealed.objects;
+
+/**
+ * The base class for all map objects like players, NPCs and whatever else we want
+ * to place on the map.
+ * 
+ * @author Drawig
+ */
+public abstract class MapObject {
+	
+	/** The x-position of this map object on the map. */
+	private int mapX;
+	/** The y-position of this map object on the map. */
+	private int mapY;
+	/** The name of the map this map object is on. */
+	private String mapName;
+	
+	/**
+	 * Constructs a new MapObject which is on the given map
+	 * on the given position.
+	 * 
+	 * @param mapX The x-position of this map object on the map.
+	 * @param mapY The y-position of this map object on the map.
+	 * @param mapName The name of the map this map object is on.
+	 */
+	public MapObject(int mapX, int mapY, String mapName) {
+		this.mapX = mapX;
+		this.mapY = mapY;
+		this.mapName = mapName;
+	}
+	
+	/**
+	 * Retrieves the x-position of this map object on the map.
+	 * 
+	 * @return The x-position of this map object on the map.
+	 */
+	public int getMapX() {
+		return mapX;
+	}
+	
+	/**
+	 * Retrieves the y-position of this map object on the map.
+	 * 
+	 * @return The y-position of this map object on the map.
+	 */	
+	public int getMapY() {
+		return mapY;
+	}
+	
+	/**
+	 * Retrieves the name of the map this map object is on.
+	 * 
+	 * @return The name of the map this map object is on.
+	 */
+	public String getMapName() {
+		return mapName;
+	}
+}
